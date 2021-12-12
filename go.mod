ccd0cfc3f32a34b7953f7b893e7cc78f00183cb3
max <maxall4@protonmail.com>
2021-12-11 16:16:41 -0800

    Init

---
+++ go.mod
@@ -0,0 +1,54 @@
+module glorb
+
+go 1.17
+
+require github.com/charmbracelet/bubbles v0.9.0
+
+require (
+	github.com/DataDog/zstd v1.4.1 // indirect
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/calmh/randomart v1.1.0 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/charmbracelet/harmonica v0.1.0 // indirect
+	github.com/charmbracelet/keygen v0.1.2 // indirect
+	github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+	github.com/dgraph-io/ristretto v0.0.4-0.20210122082011-bb5d392ed82d // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gliderlabs/ssh v0.3.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/flatbuffers v1.12.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/jacobsa/crypto v0.0.0-20190317225127-9f44e2d11115 // indirect
+	github.com/meowgorithm/babyenv v1.3.1 // indirect
+	github.com/mikesmitty/edkey v0.0.0-20170222072505-3356ea4e686a // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/go-app-paths v0.2.1 // indirect
+	github.com/muesli/sasquatch v0.0.0-20200811221207-66979d92330a // indirect
+	go.opencensus.io v0.22.5 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210326060303-6b1517762897 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+
+require (
+	github.com/atotto/clipboard v0.1.2 // indirect
+	github.com/charmbracelet/bubbletea v0.19.0 // indirect
+	github.com/charmbracelet/charm v0.9.1
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
+	github.com/charmbracelet/wish v0.1.1
+	github.com/containerd/console v1.0.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0
+	github.com/mattn/go-isatty v0.0.14-0.20210829144114-504425e14f74 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
+)
---
+++ main.go
@@ -0,0 +1,240 @@
+package main
+
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"time"
+
+	clip "github.com/atotto/clipboard"
+	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/charm/kv"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
+	"github.com/gliderlabs/ssh"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+)
+
+const host = "localhost"
+const port = 23234
+
+type tickMsg time.Time
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithMiddleware(
+			bm.Middleware(teaHandler),
+			lm.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Starting SSH server on %s:%d", host, port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server")
+	if err := s.Close(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// You can wire any Bubble Tea model up to the middleware with a function that
+// handles the incoming ssh.Session. Here we just grab the terminal info and
+// pass it to the new model. You can also return tea.ProgramOptions (such as
+// teaw.WithAltScreen) on a session by session basis
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	pty, _, active := s.Pty()
+	if !active {
+		fmt.Println("no active terminal, skipping")
+		return nil, nil
+	}
+	ti := textinput.NewModel()
+	ti.Placeholder = "Hello, World!"
+	ti.Focus()
+	ti.CharLimit = 100000 // 100,000 character limit
+	ti.Width = 20
+	// Open a database (or create one if it doesn’t exist)
+	db, err := kv.OpenWithDefaults("code-cube-pastes")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := model{
+		term:   pty.Term,
+		width:  pty.Window.Width,
+		height: pty.Window.Height,
+		textInput: ti,
+		db: db,
+		progress: progress.NewModel(progress.WithDefaultGradient()),
+	}
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	term   string
+	width  int
+	height int
+	textInput textinput.Model
+	db *kv.KV
+	savedId string
+	state string
+	progress progress.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tickMsg:
+		cmd = m.progress.IncrPercent(0.25)
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			// Close DB Instance
+			m.db.Close()
+			return m, tea.Quit
+		case "x", "X":
+			if (m.state == "") {
+				m.state = "newPaste"
+				return m, nil
+			}
+		case "r", "R":
+			if (m.state == ""){
+				 m.state = "getPaste"
+				 return m, nil
+			}
+		case "a", "A":
+			if (m.state == "") {
+				m.state = "flavor"
+			}
+			return m,nil
+		case "b", "B":
+			if (m.state == "flavor") {
+				m.state = ""
+			}
+			return m,nil
+		case "enter":
+			if (m.state == "newPaste" && m.textInput.Value() != "") {
+				// Generate ID
+				id, err := gonanoid.Generate("qwertyuiopasdfghjlzxcvbnm1234567890", 8)
+				if err != nil {
+					log.Fatal(err)
+				}
+				m.state = "loading"
+				// Save some data
+				if err := m.db.Set([]byte(id), []byte(m.textInput.Value())); err != nil {
+					log.Fatal(err)
+				}
+				// Update UI
+				m.savedId = id
+				m.state = "pasted"
+				// Close DB Instance
+				m.db.Close()
+			} else if (m.state == "getPaste" && m.textInput.Value() != "") {
+				// Fetch updates and easily define your own syncing strategy
+				if err := m.db.Sync(); err != nil {
+					log.Fatal(err)
+				}
+				m.state = "loading"
+				result, err := m.db.Get([]byte(m.textInput.Value()))
+				if (err != nil) {
+					if (err.Error() == "Key not found") {
+						m.state = "keyNotFound"
+					} else {
+						log.Fatal(err)
+					}
+				} else {
+					clip.WriteAll(string(result))
+					m.state = "copied"
+				}
+				// Close DB Instance
+				m.db.Close()
+			}
+		}
+	}
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, tea.Batch(cmd,tickCmd())
+}
+
+func (m model) View() string {
+	var title = lipgloss.NewStyle().
+    Bold(true).Foreground(lipgloss.Color("#874BFC"))
+
+	var bold = lipgloss.NewStyle().
+    Bold(true).Foreground(lipgloss.Color("#F849A3")).Underline(true)
+
+	var bold2 = lipgloss.NewStyle().
+    Bold(true).Foreground(lipgloss.Color("#F849A3"))
+
+	var enter = lipgloss.NewStyle().Foreground(lipgloss.Color("#00F3CF")).Blink(true)
+
+	var enter2 = lipgloss.NewStyle().Foreground(lipgloss.Color("#F849A3")).Blink(true)
+
+	var text = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFF")).Align(lipgloss.Center)
+
+
+	var dull = lipgloss.NewStyle().Foreground(lipgloss.Color("#878B7D"))
+
+	var renderedFlavorText = lipgloss.Place(m.width, m.height,
+			lipgloss.Center, lipgloss.Center,
+			text.Render(title.Render("About:") + "\n" + bold2.Render("CodeCube") + " is a project i made in a few hours because i was bored\n and didn't really have anything else todo.\nMade with ❤️  by Max Campbell\n" + dull.Render("Press b to go back to the home page")),
+		)
+
+		var centerMenu = lipgloss.Place(m.width, m.height,
+			lipgloss.Center, lipgloss.Center,
+			text.Render("Welcome To " + title.Render("CodeCube") + "\nThe place where you go to paste your " + bold.Render("code!") + "\n" + enter.Render("Press X to create a new Paste") + "\n" + enter2.Render("Press R to get a paste") + "\n" + dull.Render("Press a to learn more about this project")),
+		)
+		
+
+	switch m.state {
+		case "pasted":
+			return fmt.Sprintf("🚀 Paste saved!\n" + bold.Render("ID: " + m.savedId))
+		case "":
+			return fmt.Sprintf(centerMenu)
+		case "flavor":
+			return fmt.Sprintf(renderedFlavorText)
+		case "newPaste":
+			return fmt.Sprintf(enter.Render("Paste your content below:") + "\n\n%s",m.textInput.View())
+		case "copied":
+			return fmt.Sprintf(bold2.Render("🚀 Copied to your clipboard!"))
+		case "getPaste":
+			return fmt.Sprintf(enter.Render("Enter paste ID:") + "\n\n%s",m.textInput.View())
+		case "loading":
+			return m.progress.View()
+		default:
+			return fmt.Sprintf(bold.Render("Uh oh..."))
+	}
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second*1, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
